0ba99b842046e7e773c3386177c630c18caa6d0a
jorgeluis <jorgel.dominguez03@gmail.com>
2023-06-20 20:55:15 -0500

    avance

---
+++ src/main/java/com/example/lab9_20200643/Lab920200643Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20200643;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920200643Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920200643Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab9_20200643/Lab920200643ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20200643;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920200643ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
